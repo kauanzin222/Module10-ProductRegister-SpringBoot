272138d07e25163258a69456db51a06ad45b2ce7
kauanzin222 <kauaanthony.cardoso44@gmail.com>
2026-01-06 14:43:59 -0300

    Realizando um ajuste sobre a camada de serviço

--- src/main/java/com/abutua/projectbackend/resources/CategoryController.java
+++ src/main/java/com/abutua/projectbackend/resources/CategoryController.java
@@ -3,13 +3,12 @@ package com.abutua.projectbackend.resources;
 import java.util.List;
 
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.server.ResponseStatusException;
 
 import com.abutua.projectbackend.models.Category;
 import com.abutua.projectbackend.repositories.CategoryRepository;
+import com.abutua.projectbackend.services.CategoryService;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -22,12 +21,13 @@ public class CategoryController {
     @Autowired
     private CategoryRepository categoryRepository;
 
+    @Autowired 
+    private CategoryService categoryService;
+
     @GetMapping("Categories/{id}")
     public ResponseEntity<Category> getCategory(@PathVariable int id) {
 
-        Category category = categoryRepository.findById(id)
-                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Category NOT FOUND"));
-
+        Category category = categoryService.getById(id);
         return ResponseEntity.ok(category);
     }
 
