804935c60082605f608991541ad900b7f3ea6750
kauanzin222 <kauaanthony.cardoso44@gmail.com>
2026-01-06 10:46:01 -0300

    adicionando método de remoção de produtos do BD

--- src/main/java/com/abutua/projectbackend/resources/CategoryController.java
+++ src/main/java/com/abutua/projectbackend/resources/CategoryController.java
@@ -3,43 +3,35 @@ package com.abutua.projectbackend.resources;
 import java.util.List;
 
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 import com.abutua.projectbackend.models.Category;
 import com.abutua.projectbackend.repositories.CategoryRepository;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
-
+import org.springframework.web.bind.annotation.PathVariable;
 
 @RestController
 @CrossOrigin
 public class CategoryController {
 
-    
-    // private List<Category> categories = Arrays.asList(
-    //         new Category("Produção Própria"),
-    //         new Category("Nacional"),
-    //         new Category("Premium"),
-    //         new Category("Avançado"),
-    //         new Category("Importado"));
-  
-    
     @Autowired
     private CategoryRepository categoryRepository;
 
-    // @GetMapping("Categories/{id}")
-    // public ResponseEntity<Category> getCategory(@PathVariable int id) {
+    @GetMapping("Categories/{id}")
+    public ResponseEntity<Category> getCategory(@PathVariable int id) {
 
-    //     Category cat = categories.stream()
-    //             .filter(c -> c.getId() == id)
-    //             .findFirst()
-    //             .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Category not found... :c"));
+        Category category = categoryRepository.findById(id)
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Category not found... :c"));
 
-    //     return ResponseEntity.ok(cat);
-    // }
+        return ResponseEntity.ok(category);
+    }
 
-    @GetMapping("categories")
+    @GetMapping("Categories")
     public List<Category> getCategories() {
         return categoryRepository.findAll();
     }
--- src/main/java/com/abutua/projectbackend/resources/ProductController.java
+++ src/main/java/com/abutua/projectbackend/resources/ProductController.java
@@ -1,11 +1,20 @@
 package com.abutua.projectbackend.resources;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.abutua.projectbackend.models.Product;
 import com.abutua.projectbackend.repositories.ProductRepository;
@@ -14,12 +23,10 @@ import com.abutua.projectbackend.repositories.ProductRepository;
 @CrossOrigin
 public class ProductController {
 
-        /* private List<Product> products = new ArrayList<>();
-        
         @PostMapping("Products")
         public ResponseEntity<Product> save(@RequestBody Product product) {
-                product.setId(products.size() + 1);
-                products.add(product);
+
+                product = productRepository.save(product);
 
                 URI location = ServletUriComponentsBuilder
                                 .fromCurrentRequest()
@@ -30,34 +37,33 @@ public class ProductController {
                 return ResponseEntity.created(location).body(product);
         }
 
-        
         @GetMapping("Products/{id}")
         public ResponseEntity<Product> getProduct(@PathVariable int id) {
 
-                // if (id <= products.size() && id > 0)
-                // return ResponseEntity.ok(products.get(id - 1));
-                // else {
-                // throw new ResponseStatusException(HttpStatus.NOT_FOUND,"Product NOT FOUND");
-                // }
-
-                // Programação Funcional
-                Product prod = products.stream() // Transforma a lista em um fluxo de dados
-                                .filter(p -> p.getId() == id)
-                                // filter passa por CADA PRODUTO e pergunta:
-                                // o id do produto em questão é igual ao id do Path?
-                                .findFirst() // Me devolva o PRIMEIRO produto a passar no filtro!
+                Product product = productRepository.findById(id)
                                 .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND,
                                                 "Product NOT FOUND"));
-                // Caso contrário, lance a exceção. O resto do código não será lido.
 
-                return ResponseEntity.ok(prod);
-        } */
+                return ResponseEntity.ok(product);
+        }
 
         @Autowired
         public ProductRepository productRepository;
-        
+
         @GetMapping("Products")
         public List<Product> getProducts() {
                 return productRepository.findAll();
+        }
+
+        @DeleteMapping("Products/{id}")
+        public ResponseEntity<Void> removeProduct(@PathVariable int id) {
+
+                Product product = productRepository.findById(id)
+                                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND,
+                                                "Product NOT FOUND"));
+
+                productRepository.delete(product);                                                 
+
+                return ResponseEntity.noContent().build();
         }
 }
